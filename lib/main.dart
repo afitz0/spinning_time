29024a3447146602d85209ece86e3f8093fea299
Fitz <afitzgibbon@google.com>
2019-10-01 14:52:23 -0700

    spinning clock over galaxy

--- lib/main.dart
+++ lib/main.dart
@@ -21,16 +21,41 @@ class TimeMachine extends StatefulWidget {
 }
 
 class _TimeMachineState extends State<TimeMachine>
-    with SingleTickerProviderStateMixin {
-  AnimationController _animation;
+    with TickerProviderStateMixin {
+  AnimationController _rotationAnimation;
+  AnimationController _scaleAnimation;
+  AnimationController _galaxyAnimation;
+
+  Animation<double> _scaleCurve;
 
   @override
   void initState() {
     super.initState();
-    _animation = AnimationController(
+    _rotationAnimation = AnimationController(
       duration: const Duration(milliseconds: 3600),
       vsync: this,
     )..repeat();
+
+    _galaxyAnimation = AnimationController(
+      duration: const Duration(milliseconds: 14400),
+      vsync: this,
+    )..repeat();
+
+    _scaleAnimation = AnimationController(
+      duration: const Duration(milliseconds: 3600),
+      vsync: this,
+    )..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _scaleAnimation.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          _scaleAnimation.forward();
+        }
+      });
+
+    _scaleCurve =
+        CurvedAnimation(parent: _scaleAnimation, curve: Curves.easeIn);
+
+    _scaleAnimation.forward();
   }
 
   @override
@@ -39,10 +64,30 @@ class _TimeMachineState extends State<TimeMachine>
       body: Container(
         color: Colors.black,
         child: Center(
-          child: Icon(
-            Icons.hourglass_empty,
-            size: 96,
-            color: Colors.white,
+          child: Stack(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.center,
+                child: RotationTransition(
+                  turns: _galaxyAnimation,
+                  child: Image.asset('galaxy_transparent.png'),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: ScaleTransition(
+                  scale: _scaleCurve,
+                  child: RotationTransition(
+                    turns: _rotationAnimation,
+                    child: Icon(
+                      Icons.hourglass_empty,
+                      size: 192,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
